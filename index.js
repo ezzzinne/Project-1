49a671e2db42c22ec163ae997d708540d1ea8aa0
ezzzinne <cynthia.nwani017@gmail.com>
2025-08-15 14:48:46 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const identityRadios = document.querySelectorAll('input[name="identity"]');
+const moreInfo = document.getElementById('more-info');
+
+const category = {
+    name: {
+        form1: document.getElementById('input-name'),
+        form2: document.getElementById('look-up-by-name')
+    },
+    id: {
+        form1: document.getElementById('input-id'),
+        form2: document.getElementById('look-up-by-id')
+    }
+};
+
+function setFormEnabled() {
+    
+}
