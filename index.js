ad0593d7196264d1e159a9be5a5591c741e583a4
ezzzinne <cynthia.nwani017@gmail.com>
2025-08-17 17:04:42 +0100

    Built the first part of the project

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-const identityRadios = document.querySelectorAll('input[name="identity"]');
-const moreInfo = document.getElementById('more-info');
+const radios = document.querySelectorAll('input[name="identity"]');
+const checkbox = document.getElementById('more-info');
+const formA = document.getElementById('look-up-by-name');
+const formB = document.getElementById('look-up-by-id');
+const inputSection = document.getElementById('input-section');
+const inputId = document.getElementById('input-id');
+const inputLabel = document.getElementById('input-label');
 
-const category = {
-    name: {
-        form1: document.getElementById('input-name'),
-        form2: document.getElementById('look-up-by-name')
-    },
-    id: {
-        form1: document.getElementById('input-id'),
-        form2: document.getElementById('look-up-by-id')
-    }
-};
+function updateForms() {
+    try {
 
-function setFormEnabled() {
+        formA.classList.remove('active');
+        formB.classList.remove('active');
+        inputSection.classList.remove('active');
     
+        const selected = document.querySelector('input[name="identity"]:checked');
+        
+        if (!selected) return;
+
+        if (selected.value === "Name") {
+            inputLabel.textContent = 'Name';
+            inputId.placeholder = 'Enter your name';
+            inputSection.classList.add('active')
+            if (checkbox.checked) {
+                formA.classList.add('active');
+            }
+        } else if (selected.value === "Identity Number") {
+            inputLabel.textContent = 'Identity Number';
+            inputId.placeholder = 'Enter your identity number';
+            inputSection.classList.add('active')
+            if (checkbox.checked) {
+                formB.classList.add('active');
+            }
+        }
+    } catch (err) {
+        console.error("Error in updateForms:", err);
+    }
 }
+
+
+radios.forEach(radio => radio.addEventListener('change', updateForms));
+checkbox.addEventListener('change', updateForms);
+
+updateForms();
